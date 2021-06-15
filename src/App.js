b9686a747a64e1acf65c440cb3bc59ff2be424c3
FelipeSilva99 <felipe.rl.silva@hotmail.com>
2021-06-15 19:21:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  state = {
+    info: 'Poetas Brasileiros',
+    autores:['Fernando Pessoa','Machado de Assis','Clarice Lispector','Jorge Amado','Chico Buarque','Cecília Meireles']
+  }
+  
+  render(){
+    return(
+      <div className="App">
+      {this.state.autores.map((item)=>(
+        <h2>{item}</h2>
+      ))}
     </div>
   );
+  }
 }
 
 export default App;
